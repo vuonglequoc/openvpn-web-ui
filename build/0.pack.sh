8dd5c9419e21baed6813cccb06e53445a5ec87c8
Vuong Le <lelordoftech@gmail.com>
2023-01-13 22:00:26 +0900

    Upgrade base image to alpine:3.17.1

--- build/0.pack.sh
+++ build/0.pack.sh
@@ -6,5 +6,5 @@ time docker run \
     -v "$PWD/../":/go/src/github.com/vuonglequoc/openvpn-web-ui \
     --rm \
     -w /usr/src/myapp \
-    vuonglequoc/beego:2.0.5 \
+    vuonglequoc/beego:2.0.7 \
     sh -c "cd /go/src/github.com/vuonglequoc/openvpn-web-ui/ && bee version && bee pack -exr='^vendor|^data.db|^build|^README.md|^docs'"
