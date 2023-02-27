febf228f28000f15e666da16d43ea0c759904cfb
Vuong Le <lelordoftech@gmail.com>
2023-02-27 19:06:26 +0900

    Improve pack bash script

--- build/0.pack.sh
+++ build/0.pack.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+PKGFILE=openvpn-web-ui.tar.gz
+rm ../$PKGFILE
+
 time docker run \
     -v "$PWD/../":/go/src/github.com/vuonglequoc/openvpn-web-ui \
     --rm \
     -w /usr/src/myapp \
     vuonglequoc/beego:2.0.7 \
-    sh -c "cd /go/src/github.com/vuonglequoc/openvpn-web-ui/ && bee version && bee pack -exr='^vendor|^data.db|^build|^README.md|^docs'"
+    sh -c "cd /go/src/github.com/vuonglequoc/openvpn-web-ui/ && bee version && bee pack -exr='^vendor|^data.db|^build|^README.md|^docs|^.git'"
